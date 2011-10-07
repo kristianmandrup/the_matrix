798085fbc72a12857503bdb7d7fbaf72b765fee6
Kristian Mandrup <kmandrup@gmail.com>
2011-10-07 16:11:00 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheMatrix::Application.config.secret_token = 'a71061f1b4136d7551d475922f5b9fcd7340838a5e3e406f96c69f4a427c348bf71da9b21be8fa879efb337148f2066a234622fe0c39c39df93b7a6ffe53845f'
